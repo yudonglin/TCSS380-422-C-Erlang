cd19e290e0e1ee97d954cc5cfb4056a2866a5144
yudong <yudong9912@gmail.com>
2022-12-11 18:47:41 -0800

    Update helloModule.c

--- 422_A3_PageTableWalker/hello_module/helloModule.c
+++ 422_A3_PageTableWalker/hello_module/helloModule.c
@@ -12,6 +12,7 @@
 #include<linux/list.h>
 #include<linux/seq_file.h>
 #include<linux/slab.h>
+#include<asm/io.h>
 
 /** A struct data for proc report output */
 typedef struct __data
@@ -63,39 +64,53 @@ int proc_init(void)
 // Virtual address to physical address
 // pre: struct mm_struct*, unsigned long
 // post: unsigned long
-unsigned long virt2phys(const struct mm_struct *mm, unsigned long virt) 
+unsigned long virt2phys(struct mm_struct *mm, unsigned long vpage)
 {
 	/** Store the physical address */
-  	unsigned long phys = 0;
-	pgd_t *pgd;
-	pud_t *pud;
-	pmd_t *pmd;
-	pte_t *pte;
-	struct page* page;
-	pgd = pgd_offset(mm, virt);
+    pgd_t *pgd;
+    p4d_t *p4d;
+    pud_t *pud;
+    pmd_t *pmd;
+    pte_t *pte;
+    struct page *page;
+    unsigned long int physical_page_addr;
 
-	if (pgd_none(*pgd) || pgd_bad(*pgd))
-		return 0;
+    pgd = pgd_offset(mm, vpage);
 
-	pud = pud_offset(pgd, virt);
+    if (pgd_none(*pgd) || pgd_bad(*pgd)) {
+        return 0;
+    }
+    p4d = p4d_offset(pgd, vpage);
 
-	if (pud_none(*pud) || pud_bad(*pud))
-		return 0;
+    if (p4d_none(*p4d) || p4d_bad(*p4d)) {
+        return 0;
+    }
+    pud = pud_offset(p4d, vpage);
+    if (pud_none(*pud) || pud_bad(*pud)) {
+        return 0;
+    }
+    pmd = pmd_offset(pud, vpage);
 
-	pmd = pmd_offset(pud, virt);
+    if (pmd_none(*pmd) || pmd_bad(*pmd)) {
+        return 0;
+    }
+    if (!(pte = pte_offset_map(pmd, vpage))) {
+        return 0;
+    }
 
-	if (pmd_none(*pmd) || pmd_bad(*pmd))
-		return 0;
+    if (!(page = pte_page(*pte))) {
+        return 0;
+    }
 
-	if (!(pte = pte_offset_map(pmd, virt)))
-		return 0;
+    physical_page_addr = page_to_phys(page);
 
-	if (!(page = pte_page(*pte)))
-		return 0;
+    pte_unmap(pte);
 
-	phys = page_to_phys(page);
-	pte_unmap(pte);
-	return phys;
+    // handle unmapped page
+    if (physical_page_addr == 70368744173568) {
+        return 0;
+    }
+    return physical_page_addr;
 }
 
 // Iterate entire list to count all children and find out 1st child
